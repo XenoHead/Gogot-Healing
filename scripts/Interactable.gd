4850aaa127b9e700042cd54642a602fd28b12662
XenoHead <194586298+XenoHead@users.noreply.github.com>
2026-08-31 19:22:54 -0500

    dfrasfds

--- scripts/Interactable.gd
+++ scripts/Interactable.gd
@@ -2,5 +2,5 @@ extends Node3D
 class_name Interactable
 
 @export var prompt_message: String = "[E] Interact"
-@export var interaction_type: String = "" # "tv" or "couch"
-@export var interact_range: float = 2.0 # meters: how close the PLAYER must stand for the prompt to show
+@export var interaction_type: String = "" # "tv", "couch", "door_table"
+@export var interact_range: float = 1.0 # meters: how close the PLAYER must stand for the prompt to show
